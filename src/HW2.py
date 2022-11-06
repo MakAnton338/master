8529d362bdb7da7c6229136aa2eee6fdf410429e
MakAnton <Makanton95@gmail.com>
2022-11-06 15:08:51 +0200

    HW2

---
+++ src/HW2.py
@@ -0,0 +1,25 @@
+a = 2 * 3
+b = (3 * 3 + 8) / 3
+c = 8 // 3
+d = 8 % 3
+e = 5 ** 2
+f = "Hello"+"world"
+name = 'Anton'
+second_name = "Makarenko"
+g = name, second_name
+numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+h = numbers[:10]
+i = numbers[2:13]
+j = numbers[::-1]
+k = numbers[::2]
+
+rqst1 = input('First number')
+rqst2 = input('Second number')
+
+number1 = int(rqst1)
+number2 = int(rqst2)
+print(number1 + number2)
+
+
+
+print(a,b,c,d,e,f,g,h,i,j,k)
